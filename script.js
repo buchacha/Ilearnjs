d7e1090d782c1e765da54bdff5ad0afb2bfed662
buchacha <egor322129@gmail.com>
2020-09-10 17:43:46 +0000

    arrow function has no this itself

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 let user = {
-  name: "Джон",
-  hi() { alert(this.name); },
-  bye() { alert("Пока"); }
+  firstName: "Илья",
+  sayHi() {
+    let arrow = () => {alert(this.firstName)};
+    arrow();
+    alert(this.firstName)
+  }
 };
 
-user.hi(); // Джон (простой вызов метода работает хорошо)
-
-// теперь давайте попробуем вызывать user.hi или user.bye
-// в зависимости от имени пользователя user.name
-(user.name == "Джон" ? user.hi() : user.bye()); // Ошибка!
+user.sayHi(); // Илья

704c507ee83bc84c35c88748217d3bd1d8d74ae5
buchacha <egor322129@gmail.com>
2020-09-10 10:34:59 +0000

    2

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
+// $(document).ready(() => {
+//     let year = prompt('В каком году появилась спецификация ECMAScript-2015?', '');
+
+//     if (year < 2015) {
+//         alert( 'Это слишком рано...' );
+//     } else if (year > 2015) {
+//         alert( 'Это поздновато' );
+//     } else {
+//         alert( 'Верно!' );
+//     }
+// });
+
 $(document).ready(() => {
-    console.log( "ready!" );
-    result = prompt('Имя: ', ['hello']);
-    let isBoss = confirm("Ты здесь главный?");
-    alert("Hello " + result + "Boss: " + isBoss);
+    let year = prompt('В каком году появилась спецификация ECMAScript-2015?', '');
+
+    let text = (year < 2015) ? 'Это слишком рано...' 
+    : (year > 2015) ? 'Это поздновато' 
+    : 'Верно!';
+
+    alert(text)
 });

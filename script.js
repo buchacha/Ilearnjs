337cadb87220d755367e49c6d85c49025940b228
buchacha <egor322129@gmail.com>
2020-09-10 17:44:26 +0000

    example using this

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-let user = {
-  firstName: "Илья",
-  sayHi() {
-    let arrow = () => {alert(this.firstName)};
-    arrow();
-    alert(this.firstName)
+let calculator = {
+  sum() {
+    return this.a + this.b;
+  },
+
+  mul() {
+    return this.a * this.b;
+  },
+
+  read() {
+    this.a = +prompt('a?', 0);
+    this.b = +prompt('b?', 0);
   }
 };
 
-user.sayHi(); // Илья
+calculator.read();
+alert( calculator.sum() );
+alert( calculator.mul() );

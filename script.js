ac80bb9100602d05d7daa4fffa0b1d2d97f353da
buchacha <egor322129@gmail.com>
2020-09-10 13:06:34 +0000

    9

--- script.js
+++ script.js
@@ -1,23 +1,12 @@
-// function sayHi() {
-//     alert( "Привет" );
-// }
-
-function ask(question, yes, no) {
-  if (confirm(question)) yes()
-  else no();
-}
-
-function showOk() {
-  alert( "Вы согласны." );
-}
-
-function showCancel() {
-  alert( "Вы отменили выполнение." );
-}
-
 $(document).ready(() => {
 
-    ask("Вы согласны?", showOk, showCancel);
+    let age = prompt("Сколько Вам лет?", 18);
+
+    let welcome = (age < 18) ?
+    () => alert('Привет') :
+    () => alert("Здравствуйте!");
+
+    welcome(); // теперь всё в порядке
 
 });
 

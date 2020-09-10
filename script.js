a0dbdaa0ddcc6092d3464e467f239dbea38eaf1f
buchacha <egor322129@gmail.com>
2020-09-10 11:48:11 +0000

    6

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
+// $(document).ready(() => {
+
+//     browser=prompt("Какой у вас браузер?")
+
+//     if(browser == 'Edge') {
+//         alert("You've got the Edge!");
+//     } else if (browser == 'Chrome'
+//     || browser == 'Firefox'
+//     || browser == 'Safari'
+//     || browser == 'Opera') {
+//         alert( 'Okay we support these browsers too' );
+//     } else {
+//         alert( 'We hope that this page looks ok!' );
+//     }
+
+// });
+
 $(document).ready(() => {
 
-    n = prompt('Введите n', '');
-    outer: for (let i=2; i<=n; i++){
-        for (let j=2; j<i; j++){
-            if (i%j == 0) {
-                continue outer;
-            }
-        }
-        console.log(i)
+    browser=prompt("Какой у вас браузер?")
+
+    switch (browser){
+        case 'Edge' :
+            alert("You've got the Edge!");
+            alert( 'Hmm...' );
+            break;
+        case 'Firefox':
+        case 'Safari':
+        case 'Opera':
+            alert( 'Okay we support these browsers too' );
+            break;
+        default:
+            alert( 'We hope that this page looks ok!' );
+
     }
+
 });
+
 
 
 

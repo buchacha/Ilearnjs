8df954abded5598f42430901e1b0372f35e3571d
buchacha <egor322129@gmail.com>
2020-09-10 18:08:13 +0000

    function-object

--- script.js
+++ script.js
@@ -1,33 +1,14 @@
-let user = {
-  name: "John",
-  money: 1000,
-
-  [Symbol.toPrimitive](hint) {
-    return hint == "string" ? `{name: "${this.name}"}` : this.money;
+function User(name) {
+  if (!new.target) {
+    return new User(name); 
   }
-};
+  this.name = name;
 
-let user2 = {
-  name: "John",
-  money: 1000,
+  this.sayHi = function() {
+    alert( "Меня зовут: " + this.name );
+  };
+}
 
-  // для хинта равного "string"
-  toString() {
-    return `{name: "${this.name}"}`;
-  },
+let vasya = new User("Вася");
 
-  // для хинта равного "number" или "default"
-  valueOf() {
-    return this.money;
-  }
-
-};
-
-alert(user); // toString -> {name: "John"}
-alert(+user); // valueOf -> 1000
-alert(user + 500); // valueOf -> 1500
-
-// демонстрация результатов преобразований:
-alert(user2); // hint: string -> {name: "John"}
-alert(+user2); // hint: number -> 1000
-alert(user2 + 500); // hint: default -> 1500
+vasya.sayHi(); // Меня зовут: Вася

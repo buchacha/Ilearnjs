7e33467547412c8dd240476ffe4e62478ff776e5
buchacha <egor322129@gmail.com>
2020-09-10 11:34:11 +0000

    5

--- script.js
+++ script.js
@@ -1,27 +1,14 @@
-// $(document).ready(() => {
-//     let sum = 0;
-
-//     while (true) {
-
-//         let value = +prompt("Введите число", '');
-
-//         if (!value) break; // (*)
-
-//         sum += value;
-
-//     }
-//     alert( 'Сумма: ' + sum );
-// });
-
 $(document).ready(() => {
-    for (let i = 0; i < 10; i++) {
 
-        // если true, пропустить оставшуюся часть тела цикла
-        if (i % 2 == 0) continue;
-
-        alert(i); // 1, затем 3, 5, 7, 9
+    n = prompt('Введите n', '');
+    outer: for (let i=2; i<=n; i++){
+        for (let j=2; j<i; j++){
+            if (i%j == 0) {
+                continue outer;
+            }
+        }
+        console.log(i)
     }
-
 });
 
 

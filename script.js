8067bb71c697ced899985b695ca2d68e6f445cce
buchacha <egor322129@gmail.com>
2020-09-10 17:58:46 +0000

    cast object to primitive

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
-let ladder = {
-  step: 0,
-  up() {
-    this.step++;
-    return this;
-  },
-  down() {
-    this.step--;
-    return this;
-  },
-  showStep: function() { // показывает текущую ступеньку
-    alert( this.step );
-    return this;
+let user = {
+  name: "John",
+  money: 1000,
+
+  [Symbol.toPrimitive](hint) {
+    return hint == "string" ? `{name: "${this.name}"}` : this.money;
   }
 };
 
-ladder.up().up().down().showStep();
+let user2 = {
+  name: "John",
+  money: 1000,
+
+  // для хинта равного "string"
+  toString() {
+    return `{name: "${this.name}"}`;
+  },
+
+  // для хинта равного "number" или "default"
+  valueOf() {
+    return this.money;
+  }
+
+};
+
+alert(user); // toString -> {name: "John"}
+alert(+user); // valueOf -> 1000
+alert(user + 500); // valueOf -> 1500
+
+// демонстрация результатов преобразований:
+alert(user2); // hint: string -> {name: "John"}
+alert(+user2); // hint: number -> 1000
+alert(user2 + 500); // hint: default -> 1500

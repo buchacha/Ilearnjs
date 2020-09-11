273ac19cecc78c28e318b67d8612322a0b57350d
buchacha <egor322129@gmail.com>
2020-09-11 08:02:53 +0000

    random from range

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-function readNumber() {
-    let digit;
-
-    do {
-        digit = +prompt("Введите число", '');
-    } while (!digit)
-
-    return +digit;
+function random(min, max) {
+    return min + Math.random() * (max - min);
 }
 
-alert(readNumber())
+
+alert( random(1, 5) ); // 1.2345623452
+alert( random(1, 5) ); // 3.7894332423
+alert( random(1, 5) ); // 4.3435234525
+
+
